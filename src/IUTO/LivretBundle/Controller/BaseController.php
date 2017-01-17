46e7bc722f3edaab102c0a264248baf9d0ba8ac7
Grey Skipwith <greyskipwith@MacBook-Pro-de-Grey.local>
2017-01-17 12:03:37 +0100

    ajout de la fonction option dans le contrôleur de la base

--- src/IUTO/LivretBundle/Controller/BaseController.php
+++ src/IUTO/LivretBundle/Controller/BaseController.php
@@ -7,9 +7,31 @@ use Symfony\Component\HttpFoundation\Response;
 
 class BaseController extends Controller
 {
-  public function baseAction() // Test étudiant
+  public function statutAction($id) // Renvoie le statut de la personne qui se connecte
   {
-    $content = $this->get('templating')->render('IUTOLivretBundle:Base:base.html.twig', array('statutCAS' => 'étudiant'));
-    return new Response($content);
+    if ($id == 'étudiant')
+    {
+      $content = $this->get('templating')->render('IUTOLivretBundle:Base:base.html.twig', array('statutCAS' => 'étudiant'));
+      return new Response($content);
+    }
+  }
+
+  public function optionAction($id) // Renvoie les caractéristiques propre à la personne qui se connecte
+  {
+    if (statutAction($id) == "étudiant") // Si un étudiant se connecte
+    {
+      $info = $this->get('templating')->render('IUTOLivretBundle:Base:base.html.twig', array('Correction compte rendu','Créer un compte rendu'));
+      return new Response($info);
+    }
+    elseif (statutAction($id) == "professeur") // Si un professeur se connecte
+    {
+      $info = $this->get('templating')->render('IUTOLivretBundle:Base:base.html.twig', array('Demandes de correction','Projets validés'));
+      return new Response($info);
+    }
+    elseif (statutAction($id) == "communication") // Si un membre du service communication se connecte
+    {
+      $info =$this->get('templating')->render('IUTOLivretBundle:Base:base.html.twig', array('Générer un livret','Créer un édito','Corriger des projets'));
+      return new Response($info);
+    }
   }
 }
