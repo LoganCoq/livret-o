7f5eaf020b8364c748e360a49bebe2860977fbe2
Victor Maupu <victor.maupu@gmail.com>
2017-01-17 15:22:53 +0100

    modif gitignore

---
+++ src/IUTO/LivretBundle/DataFixtures/ORM/LoadCategory.php
@@ -0,0 +1,16 @@
+<?php
+// src/IUTO/LivretBundle/DataFixtures/ORM/LoadCategory.php
+
+namespace IUTO\LivretBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadCategory implements FixtureInterface
+{
+    // Dans l'argument de la méthode load, l'objet $manager est l'EntityManager
+    public function load(ObjectManager $manager)
+    {
+
+    }
+}
