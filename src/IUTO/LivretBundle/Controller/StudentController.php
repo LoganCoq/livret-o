bf3a217b323a326ed43daedf021b364fcb3f7254
Grey Skipwith <greyskipwith@MacBook-Pro-de-Grey.local>
2017-01-17 16:59:51 +0100

    création des contrôleurs pour la pagge d'accueil élève et professeur

---
+++ src/IUTO/LivretBundle/Controller/StudentController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace IUTO\LivretBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class StudentController extends Controller
+{
+  public function studentAction()
+  {
+    return $this->render('IUTOLivretBundle:Student:student.html.twig', array('statutCAS' => 'étudiant', 'options' => array('Créer un compte rendu', 'Voir corrections compte-rendu')));
+  }
+}
