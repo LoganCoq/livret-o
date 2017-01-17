be3c133f0c3ab39e3d1efbf3a28b02d01a0dcd7e
Grey Skipwith <greyskipwith@MacBook-Pro-de-Grey.local>
2017-01-17 11:06:34 +0100

    Test du remote avec Soukaina

--- src/IUTO/LivretBundle/Controller/BaseController.php
+++ src/IUTO/LivretBundle/Controller/BaseController.php
@@ -12,25 +12,25 @@ class BaseController extends Controller
     return new Response("Hello World");
   }
 
-  public function StudentAction() // récupère le status de la personne qui se connecte
+  public function StudentAction() // Test étudiant
   {
     $content = $this->get('templating')->render('LivretBundle:Default:base.html.twig', array('statutCAS' => 'étudiant'));
     return new Response($content);
   }
 
-  public function ProfAction()
+  public function ProfAction() // Test Prof
   {
     $content = $this->get('templating')->render('LivretBundle:Default:base.html.twig', array('statutCAS' => 'professeur'));
     return new Response($content);
   }
 
-  public function CommAction()
+  public function CommAction() // Test ServiceComm
   {
     $content = $this->get('templating')->render('LivretBundle:Default:base.html.twig', array('statutCAS' => 'communication'));
     return new Response($content);
   }
 
-  public function ChefAction()
+  public function ChefAction() // Test chef dpt
   {
     $content = $this->get('templating')->render('LivretBundle:Default:base.html.twig', array('statutCAS' => 'chef'));
     return new Response($content);
