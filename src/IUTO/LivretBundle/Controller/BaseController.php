caa14a28b851ef6fedcad86ccbb844cc62a29d9b
Grey Skipwith <greyskipwith@MacBook-Pro-de-Grey.local>
2017-01-17 11:13:00 +0100

    ajout de la route pour le test de la base

--- src/IUTO/LivretBundle/Controller/BaseController.php
+++ src/IUTO/LivretBundle/Controller/BaseController.php
@@ -7,32 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 
 class BaseController extends Controller
 {
-  public function baseAction() // Test du controlleur
-  {
-    return new Response("Hello World");
-  }
-
-  public function StudentAction() // Test étudiant
+  public function baseAction() // Test étudiant
   {
     $content = $this->get('templating')->render('LivretBundle:Default:base.html.twig', array('statutCAS' => 'étudiant'));
-    return new Response($content);
-  }
-
-  public function ProfAction() // Test Prof
-  {
-    $content = $this->get('templating')->render('LivretBundle:Default:base.html.twig', array('statutCAS' => 'professeur'));
-    return new Response($content);
-  }
-
-  public function CommAction() // Test ServiceComm
-  {
-    $content = $this->get('templating')->render('LivretBundle:Default:base.html.twig', array('statutCAS' => 'communication'));
-    return new Response($content);
-  }
-
-  public function ChefAction() // Test chef dpt
-  {
-    $content = $this->get('templating')->render('LivretBundle:Default:base.html.twig', array('statutCAS' => 'chef'));
     return new Response($content);
   }
 }
