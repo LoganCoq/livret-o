30fe7b92d54a8d3b217a9e6880834bea46d2a099
Soukaina <soukaina.el-abdellaoui@etu.univ-orleans.fr>
2017-01-17 14:26:15 +0100

    template base.html.twig fonctionnel : prêt pour merge request

--- src/IUTO/LivretBundle/Controller/BaseController.php
+++ src/IUTO/LivretBundle/Controller/BaseController.php
@@ -10,7 +10,7 @@ class BaseController extends Controller
   public function statutAction($id) // Renvoie le statut de la personne qui se connecte
   {
     if ($id == "étudiant"){ // Si un étudiant se connecte
-        return $this->render('IUTOLivretBundle:Base:base.html.twig', array('statutCAS' => 'étudiant', 'info' => array('Créer un compte rendu', 'Correction compte rendu')));
+      return $this->render('IUTOLivretBundle:Base:base.html.twig', array('statutCAS' => 'étudiant', 'info' => array('Créer un compte rendu', 'Correction compte rendu')));
     }
   }
 }
