3dd506a96977c59e10a229db087591edaa59e262
Grey Skipwith <greyskipwith@MacBook-Pro-de-Grey.local>
2017-01-17 13:52:16 +0100

    modification de la route et de la fonction optionAction

--- src/IUTO/LivretBundle/Controller/BaseController.php
+++ src/IUTO/LivretBundle/Controller/BaseController.php
@@ -9,28 +9,16 @@ class BaseController extends Controller
 {
   public function statutAction($id) // Renvoie le statut de la personne qui se connecte
   {
-    if ($id == 'étudiant')
-    {
       $content = $this->get('templating')->render('IUTOLivretBundle:Base:base.html.twig', array('statutCAS' => 'étudiant'));
       return new Response($content);
-    }
   }
 
   public function optionAction($id) // Renvoie les caractéristiques propre à la personne qui se connecte
   {
-    if (statutAction($id) == "étudiant") // Si un étudiant se connecte
+    echo $id;
+    if ($id == "étudiant") // Si un étudiant se connecte
     {
-      $info = $this->get('templating')->render('IUTOLivretBundle:Base:base.html.twig', array('Correction compte rendu','Créer un compte rendu'));
-      return new Response($info);
-    }
-    elseif (statutAction($id) == "professeur") // Si un professeur se connecte
-    {
-      $info = $this->get('templating')->render('IUTOLivretBundle:Base:base.html.twig', array('Demandes de correction','Projets validés'));
-      return new Response($info);
-    }
-    elseif (statutAction($id) == "communication") // Si un membre du service communication se connecte
-    {
-      $info =$this->get('templating')->render('IUTOLivretBundle:Base:base.html.twig', array('Générer un livret','Créer un édito','Corriger des projets'));
+      $info = $this->get('templating')->render('IUTOLivretBundle:Base:base.html.twig', array('info' => 'Correction compte rendu','Créer un compte rendu'));
       return new Response($info);
     }
   }
