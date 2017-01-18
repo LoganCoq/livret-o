ce1559108ec75c8143e58b1747da16b37a80f56f
joris.charlois <joris.charlois@etu.univ-orleans.fr>
2017-01-18 13:20:08 +0100

    debug numero 1 de LoadCommentaire

--- src/IUTO/LivretBundle/DataFixtures/ORM/LoadCommentaire.php
+++ src/IUTO/LivretBundle/DataFixtures/ORM/LoadCommentaire.php
@@ -14,7 +14,7 @@ class LoadCommentaire implements FixtureInterface
     // Dans l'argument de la méthode load, l'objet $manager est l'EntityManager
     public function load(ObjectManager $manager)
     {
-        $commentaire = new Personnel();
+        $commentaire = new Commentaire();
         $commentaire->setContenu("Bonjour, je ne sais pas à quoi correspond votre projet.");
         $commentaire->setReponse("Bonjour, le projet correspond à ... vous pouvez le lire dès à présent");
         $commentaire->setProjet($manager->getRepository(Projet::class)->findOneByIntituleProjet("Faire la vaisselle"));
@@ -22,7 +22,7 @@ class LoadCommentaire implements FixtureInterface
 
         $manager->persist($commentaire);
 
-        $commentaire = new Personnel();
+        $commentaire = new Commentaire();
         $commentaire->setContenu("Bonjour, vous pouvez supprmier la partie sur l'explication de l'implémentation.");
         $commentaire->setProjet($manager->getRepository(Projet::class)->findOneByIntituleProjet("Faire la vaisselle"));
         $commentaire->setsetPersonnel($manager->getRepository(Personnel::class)->findOneByMailPers("denys.duchier@univ-orleans.fr"));
