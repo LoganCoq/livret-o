60ca7f61b3c1b343bde127fc596161b35014d3f5
Logan Coquérant <logan.coquerant@gmail.com>
2017-01-18 16:50:36 +0100

    utilisation d'une instance d'étudiant pour la creation d'un projet

--- src/IUTO/LivretBundle/Controller/StudentController.php
+++ src/IUTO/LivretBundle/Controller/StudentController.php
@@ -11,4 +11,22 @@ class StudentController extends Controller
   {
     return $this->render('IUTOLivretBundle:Student:studenthome.html.twig', array('statutCAS' => 'étudiant','options' => array('Créer un compte rendu', 'Voir corrections compte-rendu')));
   }
+
+    public function createProjectAction()
+    {
+        $manager = $this->getDoctrine()->getManager();
+
+        $etudiant = $manager->getRepository(Etudiant::class)->findOneByNomEtu("Dubernet");
+
+        $formation = $etudiant->getFormation();
+
+        $anneeDebut = $formation->getYearDebut();
+        $anneeFin = $formation->getYearFin();
+
+        $departement = $formation->getDepartement();
+
+        return $this->render('IUTOLivretBundle:Student:createProject.html.twig',array('formation' => $formation),
+            array('departement' => $departement), array('anneeDebut' => $anneeDebut), array('anneeFin' => $anneeFin)
+        );
+    }
 }
