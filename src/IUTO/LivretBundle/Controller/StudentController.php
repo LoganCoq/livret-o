d15e71d09107f69f8f95aa0c15ebfcd6f73cd515
Zahori <zoe.masset@etu.univ-orleans.fr>
2017-01-18 17:21:05 +0100

    Modification des routes à cause d'erreurs

--- src/IUTO/LivretBundle/Controller/StudentController.php
+++ src/IUTO/LivretBundle/Controller/StudentController.php
@@ -7,8 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 
 class StudentController extends Controller
 {
-  public function studentAction()
+  public function studenthomeAction()
   {
-    return $this->render('IUTOLivretBundle:Student:studenthome.html.twig', array('statutCAS' => 'étudiant','options' => array('Créer un compte rendu', 'Voir corrections compte-rendu')));
+      return $this->render('IUTOLivretBundle:Student:studenthome.html.twig', array('statutCAS' => 'étudiant', 'info' => array('Créer un compte rendu', 'Correction compte rendu'),'options' => array('Créer un compte rendu', 'Voir corrections compte-rendu')));
   }
 }
