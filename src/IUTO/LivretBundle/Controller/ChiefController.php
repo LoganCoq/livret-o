77921444ed67be0b56e1d3be11457b42f4ba99e8
Cedric Clanet <cedric.clanet@gmail.com>
2017-01-18 11:52:00 +0100

    Modification des controleurs Chief et communication

--- src/IUTO/LivretBundle/Controller/ChiefController.php
+++ src/IUTO/LivretBundle/Controller/ChiefController.php
@@ -9,6 +9,17 @@ class ChiefController extends Controller
 {
   public function chiefController()
   {
-    return $this->render('IUTOLivretBundle:Chief:chief.html.twig', array('statutCAS' => 'Chef de département', 'options' => array('Générer livrets', 'Présentation département', 'Sélection des projets', 'Projets du département', 'Ajouter un projet')));
+      if ($id == "etudiant"){ // Si un étudiant se connecte
+          return $this->render('IUTOLivretBundle:Base:base.html.twig', array('statutCAS' => 'étudiant', 'info' => array('Créer un compte rendu', 'Correction compte rendu'),'options' => array('Créer un compte rendu', 'Voir corrections compte-rendu')));
+      }
+      elseif ($id == "professeur"){ // Si un professeur se connecte
+          return $this->render('IUTOLivretBundle:Base:base.html.twig', array('statutCAS' => 'professeur', 'info' => array('Demandes de correction', 'Projets validés')));
+      }
+      elseif ($id == "communication"){ // Si un membre du service communication se connecte
+          return $this->render('IUTOLivretBundle:Base:base.html.twig', array('statutCAS' => 'Service de communication', 'info' => array('Générer livrets', 'Créer un édito', 'Corriger des projets')));
+      }
+      elseif ($id == "chef"){ // Si un chef de département se connecte
+          return $this->render('IUTOLivretBundle:Base:base.html.twig', array('statutCAS' => 'Chef de département', 'info' => array('Générer livrets', 'Présentation département', 'Sélection des projets', 'Projets du département', 'Ajouter un projet'), 'options' => array('Générer un livret', 'Modifier la présentation du département', 'Sélection des projets', 'Afficher la liste des projets du département', 'Ajouter un projet')));
+      }
   }
 }
