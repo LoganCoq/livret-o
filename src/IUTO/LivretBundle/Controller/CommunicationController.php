be4ceaaa93b760ff72773f5fe6da43cb89f25083
Zahori <zoe.masset@etu.univ-orleans.fr>
2017-01-19 12:09:11 +0100

    Ajout des liens sur les boutons du menu et des pages home

--- src/IUTO/LivretBundle/Controller/CommunicationController.php
+++ src/IUTO/LivretBundle/Controller/CommunicationController.php
@@ -9,14 +9,17 @@ class CommunicationController extends Controller
 {
     public function communicationhomeAction()
     {
-        return $this->render('IUTOLivretBundle:Communication:communicationhome.html.twig', array('statutCAS' => 'service de communication', 'info' => array('Générer livrets', 'Créer un édito', 'Corriger des projets'), 'options' => array('Générer des livrets au format PDF', 'Corriger des projets', 'Créer l\'édito du directeur')));
+        return $this->render('IUTOLivretBundle:Communication:communicationhome.html.twig', array('statutCAS' => 'service de communication',
+            'info' => array('Générer livrets', 'Créer un édito', 'Corriger des projets'),
+            'options' => array('Générer des livrets au format PDF', 'Corriger des projets', 'Créer l\'édito du directeur'),
+            'routing_info' => array('#', '/communication/edito', '#'),
+            'routing_options' => array('#', '#', '/communication/edito')));
     }
 
     public function communicationeditoAction()
-
     {
-
-        return $this->render('IUTOLivretBundle:Chief:communicationedito.html.twig');
-
+        return $this->render('IUTOLivretBundle:Chief:communicationedito.html.twig', array('statutCAS' => 'service de communication',
+            'info' => array('Générer livrets', 'Créer un édito', 'Corriger des projets'),
+            'routing_info' => array('#', '#', '#')));
     }
 }
--- src/IUTO/LivretBundle/Controller/TeacherController.php
+++ src/IUTO/LivretBundle/Controller/TeacherController.php
@@ -7,24 +7,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TeacherController extends Controller
 {
-  public function teacherhomeAction()
-  {
-      return $this->render('IUTOLivretBundle:Teacher:teacherhome.html.twig', array('statutCAS' => 'professeur', 'info' => array('Demandes de correction', 'Projets validés'),'options' => array('Voir les demande de correction de projets', 'Voir les projets validés')));
-  }
+    public function teacherhomeAction()
+    {
+        return $this->render('IUTOLivretBundle:Teacher:teacherhome.html.twig', array('statutCAS' => 'professeur',
+            'info' => array('Demandes de correction', 'Projets validés'),
+            'options' => array('Voir les demande de correction de projets', 'Voir les projets validés'),
+            'routing_info' => array('#', '#'),
+            'routing_options' => array('#', '#')));
+    }
+
     public function correctionTeacher1Action($id)
     {
         $repository = $this
-
             ->getDoctrine()
-
             ->getManager()
-
-            ->getRepository('IUTOLivretBundle:Personnel')
-
-        ;
+            ->getRepository('IUTOLivretBundle:Personnel');
         $projets = $repository->findOneById($id)->getProjets();
 
-        return $this->render('IUTOLivretBundle:Teacher:correctionTeacher1.html.twig',array('id' => $id,'statutCAS' => 'professeur','projets' => $projets, 'info' => array('Demandes de correction', 'Projets validés'),'options' => array('Voir les demande de correction de projets', 'Voir les projets validés')));
+        return $this->render('IUTOLivretBundle:Teacher:correctionTeacher1.html.twig', array('id' => $id, 'statutCAS' => 'professeur', 'projets' => $projets, 'info' => array('Demandes de correction', 'Projets validés'), 'options' => array('Voir les demande de correction de projets', 'Voir les projets validés')));
 
 
     }
