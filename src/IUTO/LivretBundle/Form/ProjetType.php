4a3ca639c975faf9ee1ac9017297c4d608b4da25
Logan Coquérant <logan.coquerant@gmail.com>
2017-01-19 12:07:51 +0100

    ejout de ProjetType

---
+++ src/IUTO/LivretBundle/Form/ProjetType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace IUTO\LivretBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjetType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('intituleProjet')->add('descripProjet')->add('bilanProjet')->add('marquantProjet')->add('motsClesProjet')->add('clientProjet')->add('validerProjet')->add('dateDebut')->add('dateFin')->add('etudiants')->add('personnels')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'IUTO\LivretBundle\Entity\Projet'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'iuto_livretbundle_projet';
+    }
+
+
+}
