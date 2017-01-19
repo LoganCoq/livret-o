a3fa360661d5bc74bc05421975fd387f79adf482
Victor Maupu <victor.maupu@gmail.com>
2017-01-19 10:42:35 +0100

    Modification du TeacherController

--- src/IUTO/LivretBundle/Controller/TeacherController.php
+++ src/IUTO/LivretBundle/Controller/TeacherController.php
@@ -3,12 +3,29 @@
 namespace IUTO\LivretBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+
 
 class TeacherController extends Controller
 {
-    public function teacherhomeAction()
+  public function teacherhomeAction()
+  {
+      return $this->render('IUTOLivretBundle:Teacher:teacherhome.html.twig', array('statutCAS' => 'professeur', 'info' => array('Demandes de correction', 'Projets validés'),'options' => array('Voir les demande de correction de projets', 'Voir les projets validés')));
+  }
+    public function correctionTeacher1Action($id)
     {
-        return $this->render('IUTOLivretBundle:Teacher:teacherhome.html.twig', array('statutCAS' => 'professeur', 'info' => array('Demandes de correction', 'Projets validés'), 'options' => array('Voir les demandes de correction de projets', 'Voir les projets validés')));
+        $repository = $this
+
+            ->getDoctrine()
+
+            ->getManager()
+
+            ->getRepository('IUTOLivretBundle:Personnel')
+
+        ;
+        $projets = $repository->findOneById($id)->getProjets();
+
+        return $this->render('IUTOLivretBundle:Teacher:correctionTeacher1.html.twig',array('id' => $id,'statutCAS' => 'professeur','projets' => $projets, 'info' => array('Demandes de correction', 'Projets validés'),'options' => array('Voir les demande de correction de projets', 'Voir les projets validés')));
+
+
     }
 }
