ccd8fe734de91a7551381aa2ece64e5bd1a61dc6
Logan Coquérant <logan.coquerant@gmail.com>
2017-01-20 10:11:34 +0100

    amelioration formulaire completeProject

--- src/IUTO/LivretBundle/Form/ProjetCompleteType.php
+++ src/IUTO/LivretBundle/Form/ProjetCompleteType.php
@@ -3,11 +3,13 @@
 namespace IUTO\LivretBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProjetCompleteType extends AbstractType
 {
@@ -17,10 +19,24 @@ class ProjetCompleteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('intituleProjet', TextType::class)
-            ->add('descripProjet', TextType::class)
-            ->add('bilanProjet', TextType::class)
-            ->add('clientProjet', TextType::class)
+            ->add('intituleProjet', TextType::class, array(
+                'label' => 'Intitulé du projet',
+            ))
+            ->add('descripProjet', TextareaType::class, array(
+                'label' => 'Description',
+            ))
+            ->add('bilanProjet', TextareaType::class, array(
+                'label' => 'Bilan du projet',
+            ))
+            ->add('clientProjet', TextType::class, array(
+                'label' => 'Client'
+            ))
+            ->add('motsClesProjet', CollectionType::class, array( //TODO
+                'allow_add' => true,
+                'allow_delete' => true,
+                'prototype' => true,
+                'prototype_data' => 'New Tag Placeholder'
+            ))
             ->add('dateDebut', DateType::class)
             ->add('dateFin', DateType::class)
             ->add('submit', SubmitType::class)
