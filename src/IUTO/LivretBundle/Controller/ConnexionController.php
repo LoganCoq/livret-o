c9bab56cb5542be784f873dab31624765c86d29b
Logan Coquérant <logan.coquerant@gmail.com>
2017-03-21 10:15:52 +0100

    ajout button déconnexion

--- src/IUTO/LivretBundle/Controller/ConnexionController.php
+++ src/IUTO/LivretBundle/Controller/ConnexionController.php
@@ -21,7 +21,7 @@ class ConnexionController extends Controller
 {
     public function connexionAction()
     {
-        $numPersonne = "o2154952";
+        $numPersonne = "o2151178";
         $config = array(
             'host' => 'ldap-univ.iut45.univ-orleans.fr',
             'port' => 636,
