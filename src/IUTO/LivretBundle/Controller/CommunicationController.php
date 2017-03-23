885deb9d678416c57dbadc499bd56e7cebf09eb3
Victor Maupu <victor.maupu@gmail.com>
2017-03-23 11:00:10 +0100

    Modification de CommunicationController

--- src/IUTO/LivretBundle/Controller/CommunicationController.php
+++ src/IUTO/LivretBundle/Controller/CommunicationController.php
@@ -6,6 +6,10 @@ use Doctrine\ORM\Query\Expr\Select;
 use IUTO\LivretBundle\Entity\Livret;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Form;
+use IUTO\LivretBundle\Form\EditoType;
+use IUTO\LivretBundle\Form\ProjetCreateType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -23,14 +27,38 @@ class CommunicationController extends Controller
             'routing_options' => array('/communication/generation', '#', '/communication/selectionlivret')));
     }
 
-    public function communicationeditoAction()
+    public function communicationeditoAction(Request $request)
     {
+        $manager = $this->getDoctrine()->getManager();
+        $livret = $manager->getRepository(Livret::class)->findOneById(1); //TODO recuperation cas
+        $form = $this->createForm(EditoType::class, $livret);
+        $form->handleRequest($request);
+        $session = $this->get('session');
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('submit')->isClicked()) {
+                $ed = $form['editoLivret']->getData();
+                $livret->setEditoLivret($ed);
+                $manager->persist($livret);
+                $manager->flush();
+            }
+            if ($form->get('previsualiser')->isClicked())
+            {
+                $session->set('edito',$ed = $form['editoLivret']->getData());
+                return $this->redirectToRoute('iuto_livret_communicationEditoPrevisualiser');
+            }
+            return $this->redirectToRoute('/');
+        }
+
+
+
+
         return $this->render('IUTOLivretBundle:Communication:communicationedito.html.twig', array('statutCAS' => 'service de communication',
             'info' => array('Générer livrets', 'Créer un édito', 'Corriger des projets'),
-            'options' => array('Visualiser','Valider'),
+            'options' => array('Visualiser'),
             'routing_statutCAShome' => '/communication',
             'routing_info' => array('/communication/generation', '/communication/selectionlivret', '#'),
-            'routing_options' => array('/communication/editoprevisualiser','/communication')));
+            'routing_options' => array('/communication/editoprevisualiser'),
+            'form' => $form->createView()));
     }
 
     public function communicationgenerationlivretAction()
