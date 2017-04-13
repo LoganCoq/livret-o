d585ced8d664e6df1f05503d43cd0476e95b09f6
COQUERANT <logan.coquerant@etu.univ-orleans.fr>
2017-04-13 15:11:55 +0200

    affichage chat fonctionnel

--- src/IUTO/LivretBundle/Form/CommentaireCreateType.php
+++ src/IUTO/LivretBundle/Form/CommentaireCreateType.php
@@ -4,7 +4,7 @@ namespace IUTO\LivretBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,7 +16,7 @@ class CommentaireCreateType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('contenu', TextType::class, array(
+        ->add('contenu', TextareaType::class, array(
             'label' => 'Message',
             'attr' => [
                 'required' => true,
