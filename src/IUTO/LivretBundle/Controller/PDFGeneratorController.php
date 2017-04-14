ef5f2efa93808ae9ff3bf526eecc3b8c2afef781
COQUERANT <logan.coquerant@etu.univ-orleans.fr>
2017-04-14 12:12:44 +0200

    ajout téléchargement pdf projet

--- src/IUTO/LivretBundle/Controller/PDFGeneratorController.php
+++ src/IUTO/LivretBundle/Controller/PDFGeneratorController.php
@@ -2,10 +2,7 @@
 
 namespace IUTO\LivretBundle\Controller;
 
-use IUTO\LivretBundle\Service\HTML2PDF;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
-use IUTO\LivretBundle\Entity\Projet;
 
 
 class PDFGeneratorController extends Controller
@@ -39,7 +36,44 @@ class PDFGeneratorController extends Controller
                 'departement' => $departement,
             ]);
 
+
         $html2pdf = $this->get('app.html2pdf');
+        $html2pdf->create('P', 'A4', 'fr', true, 'UTF-8', array(10, 15, 10, 15));
+
+        return $html2pdf->generatePdf($template, "projetPDF");
+    }
+
+    public function downloadAction($id)
+    {
+        $repository = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('IUTOLivretBundle:Projet');
+
+        $projet = $repository->findOneById($id);
+
+        $nomP = $projet->getIntituleProjet();
+        $descripP = $projet->getDescripProjet();
+        $bilanP = $projet->getBilanProjet();
+        $clientP = $projet->getClientProjet();
+        $etudiants = $projet->getEtudiants();
+        $tuteurs = $projet->getTuteurs();
+        $formation = $etudiants{0}->getFormations(){0}->getTypeFormation();
+        $departement = $etudiants{0}->getFormations(){0}->getDepartement()->getNomDpt();
+
+        $template = $this->renderView('::pdf.html.twig',
+            ['nom' => $nomP,
+                'descrip' => $descripP,
+                'bilan' => $bilanP,
+                'client' => $clientP,
+                'etudiants' => $etudiants,
+                'tuteurs' => $tuteurs,
+                'formation' => $formation,
+                'departement' => $departement,
+            ]);
+
+
+        $html2pdf = $this->get('app.html2pdfdownload');
         $html2pdf->create('P', 'A4', 'fr', true, 'UTF-8', array(10, 15, 10, 15));
 
         return $html2pdf->generatePdf($template, "projetPDF");
---
+++ src/IUTO/LivretBundle/Service/HTML2PDFdownload.php
@@ -0,0 +1,24 @@
+<?php
+namespace IUTO\LivretBundle\Service;
+
+class HTML2PDFdownload
+{
+    private $pdf;
+
+    public function create($orientation=null, $format=null, $lang=null, $unicode=null, $encoding=null, $margin=null)
+    {
+        $this->pdf = new \HTML2PDF(
+            $orientation ? $orientation : $this->orientation,
+            $format ? $format : $this->format,
+            $lang ? $lang : $this->lang,
+            $unicode ? $unicode : $this->unicode,
+            $encoding ? $encoding : $this->encoding,
+            $margin ? $margin : $this->margin
+        );
+    }
+
+    public function generatePdf($template, $name){
+        $this->pdf->writeHTML($template);
+        return $this->pdf->Output($name.'.pdf', 'D');
+    }
+}
