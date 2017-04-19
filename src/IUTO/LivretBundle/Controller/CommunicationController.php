bb0650383d4369e90fc5565526acac02055e0f2b
COQUERANT <logan.coquerant@etu.univ-orleans.fr>
2017-04-19 10:19:18 +0200

    ajout namer pour upload d'image

--- src/IUTO/LivretBundle/Controller/CommunicationController.php
+++ src/IUTO/LivretBundle/Controller/CommunicationController.php
@@ -33,7 +33,7 @@ class CommunicationController extends Controller
 
         return $this->render('IUTOLivretBundle:Communication:communicationhome.html.twig', array(
             'statutCAS' => 'service de communication',
-            'info' => array('Générer livrets', 'Créer un édito', 'Corriger des projets'),
+            'info' => array('Générer livrets', 'Corriger des projets', 'Créer un édito'),
             'options' => array('Générer des livrets au format PDF', 'Corriger des projets', 'Créer l\'édito du directeur'),
             'routing_info' => array('/communication/generation', '/communication/selection', '#'),
             'routing_statutCAShome' => '/communication',
