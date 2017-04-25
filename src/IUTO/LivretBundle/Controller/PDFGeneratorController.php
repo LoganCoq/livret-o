2c1a23b2000eec82245d3f7c828809424ff049f6
COQUERANT <logan.coquerant@etu.univ-orleans.fr>
2017-04-25 14:35:58 +0200

    fonction pour l'affichage de plusieurs page en un pdf

--- src/IUTO/LivretBundle/Controller/PDFGeneratorController.php
+++ src/IUTO/LivretBundle/Controller/PDFGeneratorController.php
@@ -112,5 +112,62 @@ class PDFGeneratorController extends Controller
 
         return $html2pdf->downloadPdf($template, "projetPDF");
     }
+
+    public function generatorManyAction($projets)
+    {
+        $repository = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('IUTOLivretBundle:Projet');
+
+        $html2pdf = $this->get('app.html2pdf');
+        $html2pdf->create('P', 'A4', 'fr', true, 'UTF-8', array(10, 15, 10, 15));
+
+        foreach ( $projets as $id)
+        {
+            $projet = $repository->findOneById($id);
+
+            $nomP = $projet->getIntituleProjet();
+            $descripP = $projet->getDescripProjet();
+            $bilanP = $projet->getBilanProjet();
+            $clientP = $projet->getClientProjet();
+            $etudiants = $projet->getEtudiants();
+            $tuteurs = $projet->getTuteurs();
+            $formation = $etudiants{0}->getFormations(){0}->getTypeFormation();
+            $departement = $etudiants{0}->getFormations(){0}->getDepartement()->getNomDpt();
+
+            if ( $projet->getImages()->count() == 2 )
+            {
+                $image1 = $projet->getImages(){0};
+                $image2 = $projet->getImages(){1};
+            }
+            elseif ( $projet->getImages()->count() == 1 )
+            {
+                $image1 = $projet->getImages(){0};
+                $image2 = null;
+            }
+            else{
+                $image1 = null;
+                $image2 = null;
+            }
+
+            $template = $this->renderView('::pdf.html.twig',
+                ['nom' => $nomP,
+                    'descrip' => $descripP,
+                    'bilan' => $bilanP,
+                    'client' => $clientP,
+                    'etudiants' => $etudiants,
+                    'tuteurs' => $tuteurs,
+                    'formation' => $formation,
+                    'departement' => $departement,
+                    'image1' => $image1,
+                    'image2' => $image2,
+                ]);
+
+	        $html2pdf->writeHTML($template);
+        }
+
+        $html2pdf->Output("livret.pdf");
+    }
 }
 
