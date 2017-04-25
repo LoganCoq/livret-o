f631d10034f86c01dbb1eae2536b2da5d160bae7
COQUERANT <logan.coquerant@etu.univ-orleans.fr>
2017-04-25 14:11:29 +0200

    ajout des images au pdf

--- src/IUTO/LivretBundle/Controller/PDFGeneratorController.php
+++ src/IUTO/LivretBundle/Controller/PDFGeneratorController.php
@@ -25,6 +25,21 @@ class PDFGeneratorController extends Controller
         $formation = $etudiants{0}->getFormations(){0}->getTypeFormation();
         $departement = $etudiants{0}->getFormations(){0}->getDepartement()->getNomDpt();
 
+        if ( $projet->getImages()->count() == 2 )
+        {
+            $image1 = $projet->getImages(){0};
+            $image2 = $projet->getImages(){1};
+        }
+        elseif ( $projet->getImages()->count() == 1 )
+        {
+            $image1 = $projet->getImages(){0};
+            $image2 = null;
+        }
+        else{
+            $image1 = null;
+            $image2 = null;
+        }
+
         $template = $this->renderView('::pdf.html.twig',
             ['nom' => $nomP,
                 'descrip' => $descripP,
@@ -34,6 +49,8 @@ class PDFGeneratorController extends Controller
                 'tuteurs' => $tuteurs,
                 'formation' => $formation,
                 'departement' => $departement,
+                'image1' => $image1,
+                'image2' => $image2,
             ]);
 
 
@@ -61,6 +78,21 @@ class PDFGeneratorController extends Controller
         $formation = $etudiants{0}->getFormations(){0}->getTypeFormation();
         $departement = $etudiants{0}->getFormations(){0}->getDepartement()->getNomDpt();
 
+        if ( $projet->getImages()->count() == 2 )
+        {
+            $image1 = $projet->getImages(){0};
+            $image2 = $projet->getImages(){1};
+        }
+        elseif ( $projet->getImages()->count() == 1 )
+        {
+            $image1 = $projet->getImages(){0};
+            $image2 = null;
+        }
+        else{
+            $image1 = null;
+            $image2 = null;
+        }
+
         $template = $this->renderView('::pdf.html.twig',
             ['nom' => $nomP,
                 'descrip' => $descripP,
@@ -70,6 +102,8 @@ class PDFGeneratorController extends Controller
                 'tuteurs' => $tuteurs,
                 'formation' => $formation,
                 'departement' => $departement,
+                'image1' => $image1,
+                'image2' => $image2,
             ]);
 
 
