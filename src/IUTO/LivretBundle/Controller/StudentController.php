b63a17aabd4a7f86cca0ddb2648b123cf6b768f5
COQUERANT <logan.coquerant@etu.univ-orleans.fr>
2017-05-16 11:40:42 +0200

    maj suppressions

--- src/IUTO/LivretBundle/Controller/StudentController.php
+++ src/IUTO/LivretBundle/Controller/StudentController.php
@@ -768,7 +768,7 @@ class StudentController extends Controller
         }
 
 
-        return $this->render('IUTOLivretBundle:Student:confirmLogoDelete.html.twig', array(
+        return $this->render('IUTOLivretBundle:Student:confirmImageDelete.html.twig', array(
             'image' => $image,
             'form'   => $form->createView(),
             'statutCAS' => 'étudiant',
@@ -849,7 +849,7 @@ class StudentController extends Controller
         }
 
 
-        return $this->render('IUTOLivretBundle:Student:confirmImageDelete.html.twig', array(
+        return $this->render('IUTOLivretBundle:Student:confirmLogoDelete.html.twig', array(
             'image' => $image,
             'form'   => $form->createView(),
             'statutCAS' => 'étudiant',
