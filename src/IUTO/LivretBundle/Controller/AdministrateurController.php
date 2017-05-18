79179e26b80593a982a3e459d2492fd3f18cccc7
COQUERANT <logan.coquerant@etu.univ-orleans.fr>
2017-05-18 11:24:42 +0200

    remplissage des données choix user

--- src/IUTO/LivretBundle/Controller/AdministrateurController.php
+++ src/IUTO/LivretBundle/Controller/AdministrateurController.php
@@ -2,6 +2,7 @@
 
 namespace IUTO\LivretBundle\Controller;
 
+use IUTO\LivretBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use IUTO\LivretBundle\Entity\Projet;
 use IUTO\LivretBundle\Form\ProjetModifType;
@@ -29,12 +30,47 @@ class AdministrateurController extends Controller
 
     public function adminChooseUserAction()
     {
+        $repUser = $this->getDoctrine()->getRepository(User::class);
+        $users = $repUser->findAll();
+
+        $chiefs = array();
+        $teachers = array();
+        $students = array();
+        $employees = array();
+        $admins = array();
+        $others = array();
+        foreach ($users as $curUser)
+        {
+            $curRoles = $curUser->getRoles();
+            foreach ($curRoles as $role)
+            {
+                if ( $role == "ROLE_chief"){
+                    array_push($chiefs, $curUser);
+                } elseif ( $role == "ROLE_teacher" ) {
+                    array_push($teachers, $curUser);
+                } elseif ( $role == "ROLE_student") {
+                    array_push($students, $curUser);
+                } elseif ( $role == "ROLE_employee") {
+                    array_push($employees, $curUser);
+                } elseif ( $role == "ROLE_admin") {
+                    array_push($admins, $curUser);
+                } else {
+                    array_push($others, $curUser);
+                }
+            }
+        }
 
         return $this->render('IUTOLivretBundle:Administrateur:adminChooseUser.html.twig', array(
-            'statusCAs' => 'administrateur',
+            'statusCAS' => 'administrateur',
             'info' => array('Gérer un utilisateur'),
             'routing_info' => array("/admin/users","#"),
             'routing_statutCAShome' => 'administrateur',
+            'chiefs' => $chiefs,
+            'teachers' => $teachers,
+            'students' => $students,
+            'employees' => $employees,
+            'admins' => $admins,
+            'others' => $others,
         ));
     }
 }
