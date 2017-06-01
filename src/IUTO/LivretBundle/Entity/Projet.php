245361ccbf2aed76007fb67995d0651410c7ef30
COQUERANT <logan.coquerant@etu.univ-orleans.fr>
2017-06-01 14:14:31 +0200

    entité projet - correction nomDpt - debug syntax

--- src/IUTO/LivretBundle/Entity/Projet.php
+++ src/IUTO/LivretBundle/Entity/Projet.php
@@ -23,7 +23,7 @@ class Projet
     /**
      * @var string
      *
-     * @ORM\Column(name="nomDpt", type="string", length="255")
+     * @ORM\Column(name="nomDpt", type="string", length=255, nullable=false)
      */
     public $nomDpt;
     /**
